6d48ff6cc23c44dcc35363a2694a9fa23bba6c2f
bmv-2143 <bmv-2143@yandex.ru>
2023-07-11 16:23:45 +0300

    chore: update build gradle files

--- app/src/main/java/com/example/finalattestationreddit/MainActivity.kt
+++ app/src/main/java/com/example/finalattestationreddit/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.finalattestationreddit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.unsplashattestationproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
