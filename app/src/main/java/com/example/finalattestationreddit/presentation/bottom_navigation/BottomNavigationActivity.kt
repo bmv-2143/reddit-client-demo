b76275ca60aaaa45f7ab9ea9cf98f039cc3b36d1
bmv-2143 <bmv-2143@yandex.ru>
2023-07-14 19:37:09 +0300

    feat(uc2): configure bottom navigation with large buttons

--- app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/BottomNavigationActivity.kt
+++ app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/BottomNavigationActivity.kt
@@ -5,7 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -15,25 +16,39 @@ import com.example.unsplashattestationproject.databinding.ActivityBottomNavigati
 class BottomNavigationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBottomNavigationBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        initNavController()
+        setupBottomNavView()
+        setupActionBarWithNavController()
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_bottom_navigation)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+    private fun initNavController() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_bottom_navigation)
+                as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    private fun setupBottomNavView() {
+        val navView: BottomNavigationView = binding.activityBottomNavNavView
+        navView.setupWithNavController(navController)
+    }
+
+    private fun setupActionBarWithNavController() {
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_subreddits, R.id.navigation_favorites, R.id.navigation_user_profile
+                R.id.navigation_subreddits,
+                R.id.navigation_favorites,
+                R.id.navigation_user_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 
     companion object {
