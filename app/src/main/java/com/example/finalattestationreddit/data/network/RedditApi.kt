02c801f6c6bb71f5c395f0da529bb24f082533c2
bmv-2143 <bmv-2143@yandex.ru>
2023-07-17 13:25:04 +0300

    feat(uc2): add limit parameter to getNewSubreddits query, WIP

--- app/src/main/java/com/example/finalattestationreddit/data/network/RedditApi.kt
+++ app/src/main/java/com/example/finalattestationreddit/data/network/RedditApi.kt
@@ -2,11 +2,16 @@ package com.example.finalattestationreddit.data.network
 
 import com.example.finalattestationreddit.data.dto.ListingResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RedditApi {
 
+    // TODO: it might be better to parameterize new / popular in path with {type} and pass it as a parameter
     @GET("subreddits/new")
-    suspend fun getNewSubreddits(): ListingResponse
+    suspend fun getNewSubreddits(
+        @Query("after") after: String,
+        @Query("limit") perPage: Int
+    ): ListingResponse
 
     @GET("subreddits/popular")
     suspend fun getPopularSubreddits(): ListingResponse
