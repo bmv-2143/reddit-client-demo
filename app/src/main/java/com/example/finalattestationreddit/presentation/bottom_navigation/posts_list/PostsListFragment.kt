6b6324a92c625cc17c71e7ac503688c9c8c95a0e
bmv-2143 <bmv-2143@yandex.ru>
2023-07-18 16:04:33 +0300

    refactor(uc3): clean up PostsListFragment, add recycler view, WIP

--- app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/posts_list/PostsListFragment.kt
+++ app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/posts_list/PostsListFragment.kt
@@ -2,34 +2,18 @@ package com.example.finalattestationreddit.presentation.bottom_navigation.posts_
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.core.view.MenuHost
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.finalattestationreddit.presentation.bottom_navigation.base.ViewBindingFragment
 import com.example.unsplashattestationproject.R
 import com.example.unsplashattestationproject.databinding.FragmentPostsListBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PostsListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PostsListFragment : ViewBindingFragment<FragmentPostsListBinding>() {
-
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun inflateBinding(
         inflater: LayoutInflater,
@@ -37,76 +21,52 @@ class PostsListFragment : ViewBindingFragment<FragmentPostsListBinding>() {
     ): FragmentPostsListBinding =
         FragmentPostsListBinding.inflate(inflater, container, false)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-
-            val args = PostsListFragmentArgs.fromBundle(requireArguments())
-            Toast.makeText(requireContext(), args.subredditDisplayName, Toast.LENGTH_SHORT).show()
-        }
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolbar()
         setButtonClickListener()
+
+        val args = PostsListFragmentArgs.fromBundle(requireArguments())
+        setActionbarTitle(args.subredditDisplayName)
     }
 
     private fun initToolbar() {
         (requireActivity() as AppCompatActivity).setSupportActionBar(binding.fragmentPostsListToolbar)
         (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        setHasOptionsMenu(true)
+
+        initToolbarMenu()
     }
+
+    private fun initToolbarMenu() {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(
+            PostsListMenuProvider(
+                onNavigateUpClick = ::navigateUp,
+                onSubredditInfoMenuClick = ::navigateToSubredditInfoFragment
+            ),
+            viewLifecycleOwner,
+            Lifecycle.State.RESUMED
+        )
+    }
+
+    private fun navigateUp() {
+        requireActivity().onBackPressedDispatcher.onBackPressed()
+    }
+
+    private fun navigateToSubredditInfoFragment() {
+        findNavController().navigate(R.id.action_posts_list_fragment_to_subredditInfoFragment)
+    }
+
 
     private fun setButtonClickListener() {
-        binding.fragmentPostsListButtonOpenPost.setOnClickListener {
-            findNavController().navigate(R.id.action_postsListFragment_to_postFragment)
-        }
-    }
-
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.fragment_posts_lists_menu, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                requireActivity().onBackPressed()
-                true
-            }
-            R.id.action_fragment_menu_subreddit_info -> {
-                findNavController().navigate(R.id.action_posts_list_fragment_to_subredditInfoFragment)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+//        binding.fragmentPostsListButtonOpenPost.setOnClickListener {
+//            findNavController().navigate(R.id.action_postsListFragment_to_postFragment)
+//        }
     }
 
     private fun hideActionbar() {
         (requireActivity() as AppCompatActivity).supportActionBar?.hide()
     }
 
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PostsListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PostsListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
+
---
+++ app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/posts_list/PostsListMenuProvider.kt
@@ -0,0 +1,33 @@
+package com.example.finalattestationreddit.presentation.bottom_navigation.posts_list
+
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuProvider
+import com.example.unsplashattestationproject.R
+
+class PostsListMenuProvider(
+    private val onNavigateUpClick: () -> Unit,
+    private val onSubredditInfoMenuClick: () -> Unit
+) : MenuProvider {
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.fragment_posts_lists_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            android.R.id.home -> {
+                onNavigateUpClick()
+                true
+            }
+
+            R.id.action_fragment_menu_subreddit_info -> {
+                onSubredditInfoMenuClick()
+                true
+            }
+
+            else -> false
+        }
+    }
+}
