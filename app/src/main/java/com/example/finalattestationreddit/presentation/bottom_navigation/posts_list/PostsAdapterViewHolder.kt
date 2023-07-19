b31d5708427da50329bb24a2fb2449227ea4cb71
bmv-2143 <bmv-2143@yandex.ru>
2023-07-19 13:08:48 +0300

    feat(uc3): implement loading post image into post list item, WIP

--- app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/posts_list/PostsAdapterViewHolder.kt
+++ app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/posts_list/PostsAdapterViewHolder.kt
@@ -1,7 +1,17 @@
 package com.example.finalattestationreddit.presentation.bottom_navigation.posts_list
 
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.finalattestationreddit.data.dto.post.Post
+import com.example.finalattestationreddit.log.TAG
+import com.example.unsplashattestationproject.R
 import com.example.unsplashattestationproject.databinding.ListItemPostBinding
 
 class PostsAdapterViewHolder(
@@ -14,16 +24,7 @@ class PostsAdapterViewHolder(
 
     init {
         setPostsListItemClickListener()
-//        setSubredditSubscribeButtonClickListener()
     }
-
-//    private fun setSubredditSubscribeButtonClickListener() {
-//        binding.listItemSubredditButtonUserSubscribed.setOnClickListener {
-//            currentItem?.let {
-//                onItemSubscribeButtonClick(it)
-//            }
-//        }
-//    }
 
     private fun setPostsListItemClickListener() {
         binding.root.setOnClickListener {
@@ -36,44 +37,65 @@ class PostsAdapterViewHolder(
     fun bind(postItem: Post) {
         currentItem = postItem
         loadTexts(postItem)
-//        loadAvatar(subredditItem)
-//        loadPhoto(subredditItem.coverPhoto?.urls?.regular ?: "")
 
-//        updateSubscriptionStatusImage(postItem)
+        extractBaseImageUrl(postItem)?.let {
+            Log.e(TAG, "GLIDE_IMAGE_URL: $it")
+            loadPostImage(it)
+        }
     }
 
-//    private fun updateSubscriptionStatusImage(postItem: Post) {
-//        binding.listItemSubredditButtonUserSubscribed.setImageDrawable(
-//            if (subredditItem.userIsSubscriber) {
-//                AppCompatResources.getDrawable(
-//                    binding.root.context,
-//                    R.drawable.ic_list_item_subreddit_user_subscribed
-//                )
-//            } else {
-//                AppCompatResources.getDrawable(
-//                    binding.root.context,
-//                    R.drawable.ic_list_item_subreddit_user_not_subscribed
-//                )
-//            }
-//        )
-//    }
+    private fun extractBaseImageUrl(post: Post) : String? {
+        Log.e(TAG, "GLIDE_IMAGE_URL: images ${post.preview?.images}")
+        val url = post.preview?.images?.firstOrNull()?.source?.url
+
+        Log.e(TAG, "GLIDE_IMAGE_URL: url $url")
+
+        if (url == null) {
+            Log.e(TAG, "extractBaseImageUrl: url is null")
+            return null
+        }
+
+        val uri = Uri.parse(url)
+        val baseUrl = uri.scheme + "://" + uri.authority + uri.path
+        return baseUrl.replace("preview", "i")
+    }
+
+    private fun loadPostImage(imageUrl: String) {
+
+        Glide.with(binding.root.context)
+            .load(imageUrl)
+            .placeholder(R.drawable.list_item_post_image_placeholder_24)
+            .listener(object : RequestListener<Drawable> {
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+//                    progressBarSetVisible(binding, false)
+                    return false
+                }
+
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Log.e(TAG, "Glide onLoadFailed: $imageUrl")
+                    return false
+                }
+            })
+            .into(binding.listItemPostImage)
+    }
 
     private fun loadTexts(postItem: Post) {
-
-//        binding..text = subredditItem.id
         binding.listItemPostDisplayName.text =
             postItem.title // TODO: fix me
 
-//        if (postItem.publicDescription.isEmpty()) {
-//            binding.listItemSubredditDescription.visibility = View.GONE
-//        } else {
-//            binding.listItemSubredditDescription.visibility = View.VISIBLE
-//            binding.listItemSubredditDescription.text = subredditItem.publicDescription
-//        }
-//
-//        binding.listItemSubredditDescription.text = subredditItem.publicDescription
-
+//        binding.listItemPostDescription.text =
+//            postItem.selfText
     }
-
-
 }
