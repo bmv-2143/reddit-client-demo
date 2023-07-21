4654a8263e4cb283322a6a424b2afefd760e7af2
bmv-2143 <bmv-2143@yandex.ru>
2023-07-21 17:37:03 +0300

    refactor(uc4): clean up SubredditInfoFragment, WIP

--- app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/subreddit_info/SubredditInfoFragment.kt
+++ app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/subreddit_info/SubredditInfoFragment.kt
@@ -10,35 +10,14 @@ import com.example.finalattestationreddit.databinding.FragmentSubredditInfoBindi
 import com.example.finalattestationreddit.presentation.bottom_navigation.base.ViewBindingFragment
 import com.example.finalattestationreddit.presentation.bottom_navigation.posts_list.PostsListFragmentArgs
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SubredditInfoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SubredditInfoFragment : ViewBindingFragment<FragmentSubredditInfoBinding>() {
-
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun inflateBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentSubredditInfoBinding =
         FragmentSubredditInfoBinding.inflate(inflater, container, false)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -56,23 +35,4 @@ class SubredditInfoFragment : ViewBindingFragment<FragmentSubredditInfoBinding>(
         setHasOptionsMenu(true)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SubredditFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SubredditInfoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
