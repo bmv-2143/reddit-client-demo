6f72f3599ddd504e44519ca2fde6da5de31c90d1
bmv-2143 <bmv-2143@yandex.ru>
2023-07-22 08:53:46 +0300

    feat(uc4): load/display subreddit community icon in subreddit info scrn

--- app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/subreddit_info/SubredditInfoFragment.kt
+++ app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/subreddit_info/SubredditInfoFragment.kt
@@ -2,17 +2,18 @@ package com.example.finalattestationreddit.presentation.bottom_navigation.subred
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
 import com.example.finalattestationreddit.R
 import com.example.finalattestationreddit.data.dto.subreddit.SubredditData
 import com.example.finalattestationreddit.databinding.FragmentSubredditInfoBinding
 import com.example.finalattestationreddit.presentation.bottom_navigation.base.ViewBindingFragment
 import com.example.finalattestationreddit.presentation.bottom_navigation.posts_list.PostsListFragmentArgs
+import com.example.finalattestationreddit.presentation.utils.ImageUrlExtractor
 import com.example.finalattestationreddit.presentation.utils.ToolbarTitleSetter
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -33,19 +34,30 @@ class SubredditInfoFragment : ViewBindingFragment<FragmentSubredditInfoBinding>(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initToolbar()
 
         val subredditData = PostsListFragmentArgs.fromBundle(requireArguments()).subredditData
-        toolbarTitleSetter.setToolbarTitle(subredditData.displayNamePrefixed)
+
+        initToolbar(subredditData.displayNamePrefixed)
         setSubredditDescription(subredditData)
         setSubredditSubscriptionStatus(subredditData)
         setShareButtonClickListener(subredditData)
+        loadSubredditIcon(subredditData)
     }
 
-    private fun initToolbar() {
+    private fun loadSubredditIcon(subredditData: SubredditData) {
+        val imageUrl = ImageUrlExtractor.extractBaseImageUrl(subredditData.communityIcon)
+        Glide.with(requireContext())
+            .load(imageUrl)
+            .placeholder(R.drawable.fragment_subreddit_info_frog_face_primary_color)
+            .circleCrop()
+            .into(binding.fragmentSubredditInfoImage)
+    }
+
+    private fun initToolbar(toolbarTitle: String) {
         val activity = (requireActivity() as AppCompatActivity)
         activity.setSupportActionBar(binding.fragmentSubredditInfoToolbar)
         activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbarTitleSetter.setToolbarTitle(toolbarTitle)
     }
 
     private fun setSubredditDescription(subredditData: SubredditData) {
