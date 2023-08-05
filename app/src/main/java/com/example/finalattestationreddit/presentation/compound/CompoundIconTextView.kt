8f73c399d698e9d051c0f4e936bb1deaed0b5716
bmv-2143 <bmv-2143@yandex.ru>
2023-08-05 18:21:10 +0300

    feat(uc6): add compound icon-text line view group

---
+++ app/src/main/java/com/example/finalattestationreddit/presentation/compound/CompoundIconTextView.kt
@@ -0,0 +1,50 @@
+package com.example.unsplashattestationproject.presentation.compound
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.widget.LinearLayoutCompat
+import com.example.finalattestationreddit.R
+import com.example.finalattestationreddit.databinding.CompoundIconTextViewBinding
+
+class CompoundIconTextView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayoutCompat(context, attrs, defStyleAttr) {
+
+    private val binding = CompoundIconTextViewBinding.inflate(LayoutInflater.from(context))
+
+    init {
+        context.obtainStyledAttributes(attrs, R.styleable.CompoundIconTextView).apply {
+            text = getString(R.styleable.CompoundIconTextView_text) ?: ""
+            icon = getDrawable(R.styleable.CompoundIconTextView_icon)
+            recycle()
+        }
+        binding.root.setOnClickListener( ::onClick )
+
+        addView(binding.root)
+    }
+
+    var text: CharSequence
+        get() = binding.iconTextViewText.text
+        set(value) {
+            binding.iconTextViewText.text = value
+        }
+
+    var icon: Drawable?
+        get() = binding.iconTextViewIcon.drawable
+        set(value) {
+            binding.iconTextViewIcon.setImageDrawable(value)
+        }
+
+
+    var clickListener: OnClickListener? = null
+
+    private fun onClick(view : View) {
+        clickListener?.onClick(view)
+    }
+
+}
