6b1b7d99685f7baa527e0efd8a238c5df1ebb82a
bmv-2143 <bmv-2143@yandex.ru>
2023-08-08 15:01:01 +0300

    feat(uc7): implement all/saved subreddits toggle in favorites fragment

--- app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/favorites/FavoritesFragment.kt
@@ -1,6 +1,7 @@
 package com.example.finalattestationreddit.presentation.bottom_navigation.favorites
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.finalattestationreddit.R
 import com.example.finalattestationreddit.data.network.SubredditListType
 import com.example.finalattestationreddit.databinding.FragmentFavoritesBinding
+import com.example.finalattestationreddit.log.TAG
 import com.example.finalattestationreddit.presentation.bottom_navigation.base.ViewBindingFragment
 import com.example.finalattestationreddit.presentation.bottom_navigation.subreddits.tabs.SubredditsListFragment
 
@@ -29,8 +31,45 @@ class FavoritesFragment : ViewBindingFragment<FragmentFavoritesBinding>() {
 
         setItemClickListeners() // todo: should be updated
         addAllSubredditsFragment()
+        setSubredditsPostsToggleGroupListener()
+        setAllSavedToggleGroupListener()
     }
 
+    private fun setSubredditsPostsToggleGroupListener() {
+        binding.fragmentFavoritesToggleGroupSubredditsPosts
+            .addOnButtonCheckedListener { _, checkedId, isChecked ->
+                if (isChecked)
+                    when (checkedId) {
+                        R.id.fragment_favorites_button_subreddits -> {
+                            addAllSubredditsFragment()
+                            Log.e(TAG, "subreddits")
+                        }
+
+                        R.id.fragment_favorites_button_posts -> {
+                            Log.e(TAG, "posts")
+                            //                    addAllPostsFragment()
+                        }
+                    }
+            }
+    }
+
+    private fun setAllSavedToggleGroupListener() {
+        binding.fragmentFavoritesToggleGroupAllSaved
+            .addOnButtonCheckedListener { _, checkedId, isChecked ->
+                if (isChecked)
+                    when (checkedId) {
+                        R.id.fragment_favorites_button_saved -> {
+                            Log.e(TAG, "saved")
+                            addSubscribedSubredditsFragment()
+                        }
+
+                        R.id.fragment_favorites_button_all -> {
+                            Log.e(TAG, "all")
+                            addAllSubredditsFragment()
+                        }
+                    }
+            }
+    }
 
     private fun setItemClickListeners() {
         binding.fragmentFavoritesButtonOpenSubreddit.setOnClickListener {
@@ -58,10 +97,12 @@ class FavoritesFragment : ViewBindingFragment<FragmentFavoritesBinding>() {
 
 
     private fun addAllSubredditsFragment() {
-//        val fragment = makeAllSubredditsListFragment()
-        val fragment = makeSubscribedSubredditsListFragment()
         val transaction = childFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_favorites_fragment_container_list, fragment)
+
+        transaction.replace(
+            R.id.fragment_favorites_fragment_container_list,
+            makeAllSubredditsListFragment()
+        )
         transaction.commit()
     }
 
@@ -69,6 +110,15 @@ class FavoritesFragment : ViewBindingFragment<FragmentFavoritesBinding>() {
         return SubredditsListFragment.newInstance(
             SubredditListType.VAL_SUBREDDITS_LIST_TYPE_UNSPECIFIED
         )
+    }
+
+    private fun addSubscribedSubredditsFragment() {
+        val transaction = childFragmentManager.beginTransaction()
+        transaction.replace(
+            R.id.fragment_favorites_fragment_container_list,
+            makeSubscribedSubredditsListFragment()
+        )
+        transaction.commit()
     }
 
     private fun makeSubscribedSubredditsListFragment(): SubredditsListFragment {
