5d5f4a4e3f917dbd9dd77067c15d198964728d0b
bmv-2143 <bmv-2143@yandex.ru>
2023-08-09 19:35:51 +0300

    fix(uc7): handling received PostsListFragment arguments

--- app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/posts_list/PostsListFragment.kt
+++ app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/posts_list/PostsListFragment.kt
@@ -1,6 +1,8 @@
 package com.example.finalattestationreddit.presentation.bottom_navigation.posts_list
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,12 +14,12 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.finalattestationreddit.R
 import com.example.finalattestationreddit.data.dto.post.Post
 import com.example.finalattestationreddit.databinding.FragmentPostsListBinding
+import com.example.finalattestationreddit.log.TAG
 import com.example.finalattestationreddit.presentation.bottom_navigation.BottomNavigationViewModel
 import com.example.finalattestationreddit.presentation.bottom_navigation.base.ViewBindingFragment
 import com.example.finalattestationreddit.presentation.bottom_navigation.posts_list.PostsListType.ALL_POSTS
@@ -37,8 +39,6 @@ class PostsListFragment : ViewBindingFragment<FragmentPostsListBinding>() {
     private val viewModel: PostsListViewModel by viewModels()
     private val activityViewModel: BottomNavigationViewModel by activityViewModels()
     internal var onPostItemClickListener: WeakReference<PostItemClickListener>? = null
-
-    private val args : PostsListFragmentArgs by navArgs()
 
     @Inject
     lateinit var toolbarTitleSetter: ToolbarTitleSetter
@@ -76,27 +76,44 @@ class PostsListFragment : ViewBindingFragment<FragmentPostsListBinding>() {
     }
 
     private fun loadPosts() {
-        when (args.postsType) {
+        when (val postsType = getPostsTypeArg()) {
             SUBREDDIT_POSTS -> {
-                val subredditName = activityViewModel.selectedSubredditFlow.value?.let { subredditData ->
-                    observePostsFlow(subredditData.displayName)
+                val subredditData = activityViewModel.selectedSubredditFlow.value
+                if (subredditData != null) {
+                    viewModel.setPostsTarget(subredditData.displayName)
+                    observePostsFlow(postsType)
+                } else {
+                    Log.e(TAG, "Subreddit data is null")
                 }
             }
+
             USER_POSTS -> {
                 activityViewModel.selectedUserFlow.value?.let { userName -> // todo: filter blank user names
-                    if (userName.isNotBlank())
-                        observePostsFlow(userName)
+                    if (userName.isNotBlank()) {
+                        viewModel.setPostsTarget(userName)
+                        observePostsFlow(postsType)
+                    } else {
+                        Log.e(TAG, "User name is blank")
+                    }
                 }
             }
-            ALL_POSTS -> {
-                observePostsFlow("")
-            }
-            SAVED_POSTS -> {
-                observePostsFlow("")
-            }
-        }
 
+            ALL_POSTS, SAVED_POSTS -> {
+                observePostsFlow(postsType)
+            }
+
+            null -> Log.e(TAG, "Posts List type is null")
+        }
         observeAdapterLoadStateAndUpdateProgressBar()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun getPostsTypeArg(): PostsListType? {
+        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU)
+            arguments?.getSerializable(ARG_POSTS_TYPE) as? PostsListType
+        else
+            arguments?.getSerializable(ARG_POSTS_TYPE, PostsListType::class.java)
+
     }
 
     private fun getShowToolbarArg(): Boolean =
@@ -141,11 +158,12 @@ class PostsListFragment : ViewBindingFragment<FragmentPostsListBinding>() {
         binding.fragmentPostsListRecyclerView.adapter = postsPagingAdapter
     }
 
-    private fun observePostsFlow(subredditOrUserName: String) {
+    private fun observePostsFlow(postsType: PostsListType) {
         doOnLifecycleStarted {
-            viewModel.getPostsFlow(args.postsType, subredditOrUserName).collectLatest { pagingData ->
-                postsPagingAdapter.submitData(pagingData)
-            }
+            viewModel.getPostsFlow(postsType)
+                .collectLatest { pagingData ->
+                    postsPagingAdapter.submitData(pagingData)
+                }
         }
     }
 
@@ -170,9 +188,12 @@ class PostsListFragment : ViewBindingFragment<FragmentPostsListBinding>() {
     companion object {
 
         const val ARG_SHOW_TOOLBAR = "show_toolbar"
-        const val ARG_POSTS_TYPE = "post_type"
+        const val ARG_POSTS_TYPE = "posts_type"
 
-        fun newUserPostsInstance(showToolbar: Boolean, postItemClickListener: PostItemClickListener): PostsListFragment { // todo: which list type?
+        fun newUserPostsInstance(
+            showToolbar: Boolean,
+            postItemClickListener: PostItemClickListener
+        ): PostsListFragment { // todo: which list type?
             return PostsListFragment().apply {
                 onPostItemClickListener = WeakReference(postItemClickListener)
                 arguments = Bundle().apply {
@@ -201,6 +222,5 @@ class PostsListFragment : ViewBindingFragment<FragmentPostsListBinding>() {
                 }
             }
         }
-
     }
 }
