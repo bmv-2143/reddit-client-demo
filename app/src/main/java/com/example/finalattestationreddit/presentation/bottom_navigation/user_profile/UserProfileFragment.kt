67f83445e1ccc57cbb7d5c17dfb0ff6fd8ada3e9
bmv-2143 <bmv-2143@yandex.ru>
2023-08-11 13:44:41 +0300

    feat(uc9): load user avatar in user profile fragment

--- app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/user_profile/UserProfileFragment.kt
+++ app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/user_profile/UserProfileFragment.kt
@@ -8,9 +8,12 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.bumptech.glide.Glide
+import com.example.finalattestationreddit.R
 import com.example.finalattestationreddit.data.dto.user.User
 import com.example.finalattestationreddit.databinding.FragmentUserProfileBinding
 import com.example.finalattestationreddit.presentation.bottom_navigation.base.ViewBindingFragment
+import com.example.finalattestationreddit.presentation.utils.ImageUrlExtractor
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.filterNotNull
@@ -40,7 +43,7 @@ class UserProfileFragment : ViewBindingFragment<FragmentUserProfileBinding>() {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 userProfileViewModel.userFlow.filterNotNull().collectLatest { user ->
                     updateTexts(user)
-//                    user.iconImg?.let { loadUserAvatar(it) }
+                    user.iconImg?.let { loadUserAvatar(it) }
                 }
             }
         }
@@ -48,6 +51,15 @@ class UserProfileFragment : ViewBindingFragment<FragmentUserProfileBinding>() {
 
     private fun updateTexts(user: User) {
         binding.fragmentProfileUserName.text = user.name
+    }
+
+    private fun loadUserAvatar(avatarUrl : String) {
+        val imageUrl = ImageUrlExtractor.extractBaseImageUrl(avatarUrl)
+        Glide.with(requireContext())
+            .load(imageUrl)
+            .placeholder(R.drawable.user_placeholder_person_24)
+            .circleCrop()
+            .into(binding.fragmentUserProfileUserAvatar)
     }
 
 }
