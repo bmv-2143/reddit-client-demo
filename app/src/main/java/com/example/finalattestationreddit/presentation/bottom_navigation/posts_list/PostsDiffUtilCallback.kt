8100249aa917c870c136700126efeac39d1fecf6
bmv-2143 <bmv-2143@yandex.ru>
2023-08-14 17:11:16 +0300

    refactor: clean up line breaks in PostsDiffUtilCallback

--- app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/posts_list/PostsDiffUtilCallback.kt
+++ app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/posts_list/PostsDiffUtilCallback.kt
@@ -5,17 +5,11 @@ import com.example.finalattestationreddit.data.dto.post.Post
 
 class PostsDiffUtilCallback : DiffUtil.ItemCallback<Post>() {
 
-    override fun areItemsTheSame(
-        oldItem: Post,
-        newItem: Post
-    ): Boolean {
+    override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
         return oldItem.name == newItem.name
     }
 
-    override fun areContentsTheSame(
-        oldItem: Post,
-        newItem: Post
-    ): Boolean {
+    override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
         return oldItem == newItem
     }
 }
