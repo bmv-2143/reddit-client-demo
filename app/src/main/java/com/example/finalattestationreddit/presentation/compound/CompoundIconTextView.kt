223e99f16c6f3a9cb1eee37a91351faec6f18bab
bmv-2143 <bmv-2143@yandex.ru>
2023-08-14 14:48:20 +0300

    refactor: clean up compound icon text view members order, package

--- app/src/main/java/com/example/finalattestationreddit/presentation/compound/CompoundIconTextView.kt
+++ app/src/main/java/com/example/finalattestationreddit/presentation/compound/CompoundIconTextView.kt
@@ -1,4 +1,4 @@
-package com.example.unsplashattestationproject.presentation.compound
+package com.example.finalattestationreddit.presentation.compound
 
 import android.content.Context
 import android.graphics.drawable.Drawable
@@ -16,6 +16,7 @@ class CompoundIconTextView @JvmOverloads constructor(
 ) : LinearLayoutCompat(context, attrs, defStyleAttr) {
 
     private val binding = CompoundIconTextViewBinding.inflate(LayoutInflater.from(context))
+    private var clickListener: OnClickListener? = null
 
     init {
         context.obtainStyledAttributes(attrs, R.styleable.CompoundIconTextView).apply {
@@ -39,9 +40,6 @@ class CompoundIconTextView @JvmOverloads constructor(
         set(value) {
             binding.iconTextViewIcon.setImageDrawable(value)
         }
-
-
-    var clickListener: OnClickListener? = null
 
     private fun onClick(view : View) {
         clickListener?.onClick(view)
