07e728a1d8252fcef8cbef202ef2aa51151c24ad
bmv-2143 <bmv-2143@yandex.ru>
2023-08-14 16:46:04 +0300

    refactor: remove comments, commented code in SubredditsAdapterViewHolder

--- app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/subreddits/tabs/SubredditsAdapterViewHolder.kt
+++ app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/subreddits/tabs/SubredditsAdapterViewHolder.kt
@@ -4,8 +4,8 @@ import android.view.View
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.RecyclerView
 import com.example.finalattestationreddit.R
-import com.example.finalattestationreddit.databinding.ListItemSubredditBinding
 import com.example.finalattestationreddit.data.dto.subreddit.SubredditData
+import com.example.finalattestationreddit.databinding.ListItemSubredditBinding
 
 class SubredditsAdapterViewHolder(
     private val binding: ListItemSubredditBinding,
@@ -39,9 +39,6 @@ class SubredditsAdapterViewHolder(
     fun bind(subredditItem: SubredditData) {
         currentItem = subredditItem
         loadTexts(subredditItem)
-//        loadAvatar(subredditItem)
-//        loadPhoto(subredditItem.coverPhoto?.urls?.regular ?: "")
-
         updateSubscriptionStatusImage(subredditItem)
     }
 
@@ -64,8 +61,6 @@ class SubredditsAdapterViewHolder(
     }
 
     private fun loadTexts(subredditItem: SubredditData) {
-
-//        binding..text = subredditItem.id
         binding.listItemSubredditDisplayName.text =
             subredditItem.displayNamePrefixed // TODO: fix me
 
@@ -77,67 +72,5 @@ class SubredditsAdapterViewHolder(
         }
 
         binding.listItemSubredditDescription.text = subredditItem.publicDescription
-
-//        binding.collectionListItemNumberOfPhotos.text =
-//            binding.root.context.getString(
-//                R.string.collection_item_number_of_photos_template,
-//                collectionItem.totalPhotos.toString()
-//            )
-//
-//        binding.collectionListItemTitle.text = collectionItem.title
-//        binding.collectionAuthorName.text = collectionItem.user.name
-//
-//        binding.collectionAuthorNickname.text =
-//            binding.root.context.getString(
-//                R.string.photo_item_author_nickname_template,
-//                collectionItem.user.username
-//            )
     }
-
-//    private fun loadAvatar(collectionItem: UnsplashCollection) {
-//        Glide.with(binding.root)
-//            .load(collectionItem.user.profileImageUrls.small)
-//            .circleCrop()
-//            .placeholder(R.drawable.photo_list_item_avatar_placeholder)
-//            .into(binding.collectionAuthorAvatar)
-//    }
-//
-//    private fun loadPhoto(imageUrl: String) {
-//        progressBarSetVisible(binding, true)
-//
-//        Glide.with(binding.root.context)
-//            .load(imageUrl)
-//            .placeholder(R.drawable.photo_list_item_image_placeholder)
-//            .listener(object : RequestListener<Drawable> {
-//
-//                override fun onResourceReady(
-//                    resource: Drawable?,
-//                    model: Any?,
-//                    target: Target<Drawable>?,
-//                    dataSource: DataSource?,
-//                    isFirstResource: Boolean
-//                ): Boolean {
-//                    progressBarSetVisible(binding, false)
-//                    return false
-//                }
-//
-//                override fun onLoadFailed(
-//                    e: GlideException?,
-//                    model: Any?,
-//                    target: Target<Drawable>?,
-//                    isFirstResource: Boolean
-//                ): Boolean {
-//                    Log.e(TAG, "Glide onLoadFailed: $imageUrl")
-//                    return false
-//                }
-//            })
-//            .into(binding.collectionItemImage)
-//    }
-//
-//    private fun progressBarSetVisible(binding: CollectionListItemBinding, isActive: Boolean) =
-//        if (isActive) {
-//            binding.collectionItemProgressBar.visibility = android.view.View.VISIBLE
-//        } else {
-//            binding.collectionItemProgressBar.visibility = android.view.View.GONE
-//        }
 }
