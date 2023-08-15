439047bdd9c95a233d6ffc57d9e2ab19bd3f43ed
bmv-2143 <bmv-2143@yandex.ru>
2023-08-15 14:05:38 +0300

    refactor: extract string constants in AuthInterceptor

--- app/src/main/java/com/example/finalattestationreddit/di/AuthInterceptor.kt
+++ app/src/main/java/com/example/finalattestationreddit/di/AuthInterceptor.kt
@@ -7,9 +7,14 @@ class AuthInterceptor(private val tokenProvider: TokenProvider) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
-            .addHeader("Authorization", "Bearer ${tokenProvider.getToken()}")
+            .addHeader(AUTHORIZATION_HEADER, "$BEARER_PREFIX ${tokenProvider.getToken()}")
             .build()
         return chain.proceed(request)
+    }
+
+    companion object {
+        private const val AUTHORIZATION_HEADER = "Authorization"
+        private const val BEARER_PREFIX = "Bearer"
     }
 
 }
