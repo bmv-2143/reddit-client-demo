a06af5a388308181eb80df3daa994a51b33dfc15
bmv-2143 <bmv-2143@yandex.ru>
2023-08-15 19:16:07 +0300

    refactor: clean up PagingSourceMappers

--- app/src/main/java/com/example/finalattestationreddit/data/pagingsource/PagingSourceMappers.kt
+++ app/src/main/java/com/example/finalattestationreddit/data/pagingsource/PagingSourceMappers.kt
@@ -2,22 +2,10 @@ package com.example.finalattestationreddit.data.pagingsource
 
 import com.example.finalattestationreddit.data.model.dto.post.PostData
 import com.example.finalattestationreddit.data.model.dto.post.PostListingData
-import com.example.finalattestationreddit.data.model.dto.post.PostListingResponse
-import com.example.finalattestationreddit.data.model.dto.subreddit.SubredditListingData
-import com.example.finalattestationreddit.data.model.dto.subreddit.SubredditListingResponse
 import com.example.finalattestationreddit.data.model.dto.subreddit.SubredditData
+import com.example.finalattestationreddit.data.model.dto.subreddit.SubredditListingData
 
 
-fun SubredditListingResponse.toSubredditDataList() : List<SubredditData> =
-    this.data.toSubredditDataList()
+fun SubredditListingData.toSubredditDataList(): List<SubredditData> = this.children.map { it.data }
 
-// todo: should be here?  in dto?
-fun SubredditListingData.toSubredditDataList(): List<SubredditData> =
-    this.children.map { it.data }
-
-fun PostListingResponse.toPostDataList() : List<PostData> =
-    this.data.toPostDataList()
-
-// todo: should be here?  in dto?
-fun PostListingData.toPostDataList(): List<PostData> =
-    this.children
+fun PostListingData.toPostDataList(): List<PostData> = this.children
