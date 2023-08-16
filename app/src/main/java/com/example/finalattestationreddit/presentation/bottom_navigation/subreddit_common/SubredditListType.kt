8cda88ec5526c64037823edad2719e5337e53463
bmv-2143 <bmv-2143@yandex.ru>
2023-08-16 13:40:52 +0300

    refactor: remove unused const from SubredditListType

--- app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/subreddit_common/SubredditListType.kt
+++ app/src/main/java/com/example/finalattestationreddit/presentation/bottom_navigation/subreddit_common/SubredditListType.kt
@@ -5,7 +5,6 @@ object SubredditListType {
     const val ARG_SUBREDDITS_LIST_TYPE = "subredditsListType"
     const val VAL_SUBREDDITS_LIST_TYPE_NEW = "new"
     const val VAL_SUBREDDITS_LIST_TYPE_POPULAR = "popular"
-    const val VAL_SUBREDDITS_LIST_TYPE_DEFAULT = "default"
     const val VAL_SUBREDDITS_LIST_TYPE_UNSPECIFIED = ""
     const val VAL_SUBREDDITS_LIST_TYPE_SUBSCRIBED = "subscribed"
 
